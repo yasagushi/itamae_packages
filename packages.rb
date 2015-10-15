332271a2389234c67105f5f688f30cc2d921a4cf
Yasuo Sasaki <y-sasaki@forcus.xyz>
2015-10-15 00:11:26 +0000

    services separate

--- packages.rb
+++ packages.rb
@@ -6,17 +6,3 @@ node['packages'].length.times do |i|
   package node['packages'][i]
   user "root"
 end
-
-node['services']['on'].length.times do |i|
-  service node['services']['on'][i] do
-    action [:start, :enable]
-    user "root"
-  end
-end
-
-node['services']['off'].length.times do |i|
-  service node['services']['off'][i] do
-    action [:stop, :disable]
-    user "root"
-  end
-end
